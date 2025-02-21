80767cd949e62f16c877eccdf793f1a45fad0294
redaelemawy <redaelemawy98@gmail.com>
2025-02-21 12:09:42 +0200

    feat: auto-importing utils

--- components/Test.vue
+++ components/Test.vue
@@ -1,25 +1,7 @@
 <template>
   <div>
-    <h1>Current Route</h1>
+    <h1>This is - {{ getName() }}</h1>
   </div>
 </template>
 
-<script setup>
-/** invalid code throw errors */
-// async function invalidUsage() {
-//   const response = await useAsyncData(
-//     () => new Promise((resolve) => setTimeout(resolve('hamo'), 6000))
-//   ); // Delayed async call
-//   useHead({ title: 'hakko' });
-// }
-// await invalidUsage(); // not working error
-
-/** working code */
-async function invalidUsage() {
-  const response = await useAsyncData(
-    () => new Promise((resolve) => setTimeout(resolve('hamo'), 6000))
-  ); // Delayed async call
-  useHead({ title: response.data });
-}
-invalidUsage(); // will work without errors
-</script>
+<script setup></script>
