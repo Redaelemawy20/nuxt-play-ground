58e714dc9f23f61d1c2321e23e9e56639e171a60
redaelemawy <redaelemawy98@gmail.com>
2025-02-21 12:24:52 +0200

    feet: importing default imports using '#imports'

--- components/Test.vue
+++ components/Test.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>This is - {{ getName() }}</h1>
+    <p>api is - {{ api }}</p>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { api, getName } from '#imports';
+console.log(api);
+</script>
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,1 +1,2 @@
 export const getName = () => 'reda king';
+export const api = 'xyz.com/api';
