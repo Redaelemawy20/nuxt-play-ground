eca2c0b38566213f447970432da1164e7b7e3069
redaelemawy <redaelemawy98@gmail.com>
2025-02-21 12:08:16 +0200

    test: test invalid using of composables

---
+++ components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Current Route</h1>
+  </div>
+</template>
+
+<script setup>
+/** invalid code throw errors */
+// async function invalidUsage() {
+//   const response = await useAsyncData(
+//     () => new Promise((resolve) => setTimeout(resolve('hamo'), 6000))
+//   ); // Delayed async call
+//   useHead({ title: 'hakko' });
+// }
+// await invalidUsage(); // not working error
+
+/** working code */
+async function invalidUsage() {
+  const response = await useAsyncData(
+    () => new Promise((resolve) => setTimeout(resolve('hamo'), 6000))
+  ); // Delayed async call
+  useHead({ title: response.data });
+}
+invalidUsage(); // will work without errors
+</script>
