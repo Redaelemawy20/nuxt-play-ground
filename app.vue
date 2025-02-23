6cd67b7e222f1e8432febfd81ca5e0048cc67a3a
redaelemawy <redaelemawy98@gmail.com>
2025-02-23 22:55:38 +0200

    watch reactive object

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <Test />
+    <Watchers />
   </div>
 </template>
---
+++ components/Watchers.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>watchers</h1>
+</template>
+
+<script lang="ts">
+const obj = reactive({ count: 0 });
+
+watch(obj, (newValue, oldValue) => {
+  console.log('New Value:', newValue.count);
+  console.log('Old Value:', oldValue.count);
+});
+
+obj.count++;
+</script>
