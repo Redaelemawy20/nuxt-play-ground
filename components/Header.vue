40c99d68f1045c4df1b193844868aafd356cb459
redaelemawy <redaelemawy98@gmail.com>
2025-05-14 14:57:58 +0300

    component auto import

---
+++ components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Our Header</h1>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,8 @@
 <template>
   <div>
-    <h1>Posts -- {{ a.name.length.dk.d }}</h1>
-    <ul>
-      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
-    </ul>
+    <Header></Header>
+    <p>This our home content</p>
+    <UiButtonsMainBtn>click here</UiButtonsMainBtn>
   </div>
 </template>
-<script setup>
-async function fetchPosts() {
-  try {
-    const data = await $fetch('https://jsonplaceholder.typicsode.com/posts');
-    return data;
-  } catch (err) {
-    // Automatically trigger Nuxt error page
-    // throw createError({
-    //   statusCode: 500,
-    //   statusMessage: 'Failed to fetch posts!',
-    // });
-  }
-}
-const posts = await fetchPosts();
-let a = {
-  name: 'reda',
-};
-</script>
+<script setup></script>
